27007287ec3524cb8dd4473346b3f8e1f16b7553
thedhruvish <thedhruvish@gmail.com>
2026-02-06 16:58:12 +0530

    Init project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () =>
+  console.log(`server are the runing http://localhost:${PORT}`),
+);
